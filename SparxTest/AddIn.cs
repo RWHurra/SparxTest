0bfe0e91d985f296f048dcf576b07b34d59dcd93
RWHurra <robert.hurra@arway.se>
2016-09-20 09:38:37 +0200

    Added count and message box
    Added code to count classes and a message box to show the count.

--- SparxTest/AddIn.cs
+++ SparxTest/AddIn.cs
@@ -23,5 +23,54 @@ namespace SparxTest
             }
             return "";
         }
+
+        static bool IsProjectOpen(Repository repository)
+        {
+            try
+            {
+                return null != repository.Models;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void EA_GetMenuState(Repository repository, string location, string menuName, string itemName, ref bool isEnabled, ref bool isChecked)
+        {
+            if (IsProjectOpen(repository))
+            {
+                if (itemName == "My Add-In!")
+                    isEnabled = true;
+            }
+            else
+                isEnabled = false;
+        }
+
+        public void EA_MenuClick(Repository repository, string location, string menuName, string itemName)
+        {
+            switch (itemName)
+            {
+                case "My Add-In!":
+                    var count = 0;
+                    foreach (Package model in repository.Models)
+                        foreach (Package package in model.Packages)
+                            count += CountClasses(package);
+                    MessageBox.Show("this project contains "
+                        + count + " " + (count == 1 ? "class" : "classes"));
+                    break;
+            }
+        }
+
+        private static int CountClasses(Package package)
+        {
+            var count = 0;
+            foreach (Element e in package.Elements)
+                if (e.Type == "Class")
+                    count++;
+            foreach (Package p in package.Packages)
+                count += CountClasses(p);
+            return count;
+        }
     }
 }
