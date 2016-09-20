9244176f11645d2573340d6be4407c5d2b31377f
RWHurra <robert.hurra@arway.se>
2016-09-20 09:15:26 +0200

    Added Visual Studio Project
    I tried to add the VS project. Did it work?

---
+++ SparxTest/AddIn.cs
@@ -0,0 +1,27 @@
+﻿using EA;
+using System.Runtime.InteropServices;
+
+namespace SparxTest
+{
+    [ComVisible(true)]
+    public class AddIn
+    {
+        // Called Before EA starts to check Add-In Exists
+        public string EA_Connect(Repository repository)
+        {
+            // nothing special
+            return "SparxTest.AddIn - connected";
+        }
+        // Called when user Click Add-Ins Menu item.
+        public object EA_GetMenuItems(Repository repository,
+            string location, string menuName)
+        {
+            switch (menuName)
+            {
+                case "":
+                    return "My Add-In!";
+            }
+            return "";
+        }
+    }
+}
